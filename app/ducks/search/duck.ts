e7cc1a64cf1ee3f652cd744c2f2164e546fda41f
Dpbm <dpbm136@gmail.com>
2024-10-27 09:50:17 -0300

    style: finished search alien page

--- app/ducks/search/duck.ts
+++
@@ -1,6 +0,0 @@
-export function randomDuck():boolean{
-    // 0 = normal duck
-    // 1 = xenophagus (60% of chance)
-
-    return Math.random() >= 0.6;
-}
--- app/ducks/search/page.tsx
+++ app/ducks/search/page.tsx
@@ -2,70 +2,70 @@
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import { useEffect, useState } from 'react';
-import { randomDuck } from './duck';
 import { redirect } from 'next/navigation';
 
-export default function SearchDuck(){
-    const [searching, setSearching] = useState(true);
-    const [scanning, setScanning] = useState(false);
+import './page.styles.css';
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setSearching(false);
-            setScanning(true);
-        }, 4000);
+export default function SearchDuck() {
+	const [searching, setSearching] = useState(true);
+	const [scanning, setScanning] = useState(false);
 
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [])
+	function randomDuck(): boolean {
+		// 0 = normal duck
+		// 1 = alien (60% of chance)
+		return Math.random() <= 0.6;
+	}
 
-    useEffect(() => {
-        if(!scanning) return () => {};
-        const timeout = setTimeout(() => {
-            setScanning(false);
-            const isAXenophagus = randomDuck();
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setSearching(false);
+			setScanning(true);
+		}, 4000);
 
-            if(isAXenophagus)
-                redirect('/ducks/search/found/xenophagus');
-            else
-                redirect('/ducks/search/found/duck');
-        }, 4000);
-        
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, []);
 
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [scanning]);
+	useEffect(() => {
+		if (!scanning) return () => {};
+		const timeout = setTimeout(() => {
+			setScanning(false);
+			const isAXenophagus = randomDuck();
 
+			if (isAXenophagus) redirect('/ducks/search/found/alien');
+			else redirect('/ducks/search/found/duck');
+		}, 4000);
 
-    if(searching){
-        return (
-            <>
-                <DotLottieReact 
-                    loop 
-                    autoplay
-                    style={{ height: '300px', width: '300px' }}
-                    src="/searching-duck.lottie"/>
-                <p>Procurando patos suspeitos...</p>
-            </>
-        );
-    }
-        
-    return(
-        <>
-            <h1>Pato encontrado</h1>
-            <DotLottieReact 
-                loop 
-                autoplay
-                style={{ height: '300px', width: '300px' }}
-                src="/scanning-duck.lottie"/>
-            <p>Escaneando pato...</p>
-        </>
-    );
-    
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [scanning]);
 
-    
+	if (searching) {
+		return (
+			<main>
+				<DotLottieReact
+					loop
+					autoplay
+					style={{ height: '300px', width: '300px' }}
+					src='/searching-duck.lottie'
+				/>
+				<p>Procurando patos suspeitos...</p>
+			</main>
+		);
+	}
 
-
+	return (
+		<main>
+			<h1>Pato encontrado</h1>
+			<DotLottieReact
+				loop
+				autoplay
+				style={{ height: '300px', width: '300px' }}
+				src='/scanning-duck.lottie'
+			/>
+			<p>Escaneando pato...</p>
+		</main>
+	);
 }
