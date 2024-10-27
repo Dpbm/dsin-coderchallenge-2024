4413d9d9a5784ba3d69e64559a15a8ee10761893
Dpbm <dpbm136@gmail.com>
2024-10-27 09:57:23 -0300

    style: finished found duck

--- app/ducks/search/found/duck/page.tsx
+++ app/ducks/search/found/duck/page.tsx
@@ -2,21 +2,22 @@
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
+import './page.styles.css';
 
-export default function Duck(){
-    return (
-        <>
-            <DotLottieReact 
-            loop 
-            autoplay
-            style={{ height: '300px', width: '300px' }}
-            src="/clear.lottie"/>
-            <h1>Ufa, é apenas um pato comum 🙃</h1>
-            <a href='/ducks/search'>Procurar mais!</a>
-            <a href='/ducks'>Voltar ao inicio!</a>
-        </>
-        
-        
-    );
-
+export default function Duck() {
+	return (
+		<main>
+			<DotLottieReact
+				loop
+				autoplay
+				style={{ height: '300px', width: '300px' }}
+				src='/clear.lottie'
+			/>
+			<h1>🙃Ufa, é apenas um pato comum🙃</h1>
+			<div id='actions'>
+				<a href='/ducks/search'>Procurar mais!</a>
+				<a href='/'>Voltar ao inicio!</a>
+			</div>
+		</main>
+	);
 }
